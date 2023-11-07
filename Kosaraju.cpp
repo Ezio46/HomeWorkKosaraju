a6206621ab0f9b9333d50e7a19c3ebbe85da6f45
Ezio4 <62401007+Ezio46@users.noreply.github.com>
2023-11-07 19:14:19 +0300

    Update Kosaraju.cpp
    add cost crit

--- Kosaraju.cpp
+++ Kosaraju.cpp
@@ -8,13 +8,13 @@ using namespace std;
 class Graph {
 public:
     Graph(int vertices);
-    void addEdge(int from, int to);
+    void addEdge(int from, int to, int weight);
     vector<int> getSCC();
     
 private:
     int vertices;
-    vector<vector<int>> adj;
-    vector<vector<int>> transpose;
+    vector<vector<pair<int, int>> > adj; 
+    vector<vector<pair<int, int>> > transposed;
     vector<bool> visited;
     stack<int> order;
     unordered_set<int> currentComponent;
@@ -27,14 +27,14 @@ private:
 Graph::Graph(int vertices) {
     this->vertices = vertices;
     adj.resize(vertices);
-    transpose.resize(vertices);
+    transposed.resize(vertices);
     visited.assign(vertices, false);
     components.resize(vertices);
 }
 
-void Graph::addEdge(int from, int to) {
-    adj[from].push_back(to);
-    transpose[to].push_back(from);
+void Graph::addEdge(int from, int to, int weight) {
+    adj[from].push_back(make_pair(to, weight));
+    transposed[to].push_back(make_pair(from, weight));
 }
 
 vector<int> Graph::getSCC() {
@@ -70,11 +70,11 @@ void Graph::DFS(int v, bool saveState) {
         currentComponent.insert(v);
     }
     
-    vector<int>& neighbors = (saveState) ? transpose[v] : adj[v];
+    vector<pair<int, int>>& neighbors = (saveState) ? transposed[v] : adj[v];
     
-    for (int neighbor : neighbors) {
-        if (!visited[neighbor]) {
-            DFS(neighbor, saveState);
+    for (const pair<int, int>& neighbor : neighbors) {
+        if (!visited[neighbor.first]) {
+            DFS(neighbor.first, saveState);
         }
     }
 }
@@ -82,9 +82,9 @@ void Graph::DFS(int v, bool saveState) {
 void Graph::fillOrder(int v) {
     visited[v] = true;
     
-    for (int neighbor : adj[v]) {
-        if (!visited[neighbor]) {
-            fillOrder(neighbor);
+    for (const pair<int, int>& neighbor : adj[v]) {
+        if (!visited[neighbor.first]) {
+            fillOrder(neighbor.first);
         }
     }
     
@@ -95,16 +95,16 @@ int main() {
     int vertices = 8;
     Graph g(vertices);
     
-    g.addEdge(0, 1);
-    g.addEdge(1, 2);
-    g.addEdge(2, 1);
-    g.addEdge(2, 3);
-    g.addEdge(3, 4);
-    g.addEdge(4, 3);
-    g.addEdge(5, 4);
-    g.addEdge(6, 5);
-    g.addEdge(7, 6);
-    g.addEdge(6, 7);
+    g.addEdge(0, 1, 1);
+    g.addEdge(1, 2, 2);
+    g.addEdge(2, 0, 3);
+    g.addEdge(2, 3, 4);
+    g.addEdge(3, 4, 5);
+    g.addEdge(4, 3, 6);
+    g.addEdge(5, 4, 7);
+    g.addEdge(6, 5, 8);
+    g.addEdge(7, 6, 9);
+    g.addEdge(6, 7, 10);
     
     vector<int> components = g.getSCC();
     
